b0599343acc09c9fbb7dbe3ee9f380d1a2cdead9
manish prajapati <test@gmail.com>
2022-07-26 11:28:12 +0530

    just little changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import {NavBar, Footer, Home, Services, About, Contact, Signup, Login, Error404} from "./Components";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+let home = "https://manish202.github.io/manishpay";
 function App(){
   return <BrowserRouter>
     <NavBar />
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
+      <Route path={home+"/"} element={<Home />} />
+      <Route path={home+"/services"} element={<Services />} />
+      <Route path={home+"/about"} element={<About />} />
+      <Route path={home+"/contact"} element={<Contact />} />
+      <Route path={home+"/signup"} element={<Signup />} />
+      <Route path={home+"/login"} element={<Login />} />
       <Route path="*" element={<Error404 />} />
     </Routes>
     <Footer />
